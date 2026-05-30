47712b149960b450b3787692fbac156af16ebb1c
Gustavo Blas Duran <42410146+gblasd@users.noreply.github.com>
2026-05-29 23:49:31 -0600

    Update test.py
    Update test file, files not found

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ from init_db import extract_load_data
 def test_db():
 
     # Extract data and load into database if not exists
-    assert os.path.exists('db/airbnb.db') is True
+    assert os.path.exists('db/airbnb.db') is False
 
     # Transform text listings and create embeddings, save to file npy if not exists
-    assert os.path.exists('db/text_embeddings.npy') is True
+    assert os.path.exists('db/text_embeddings.npy') is False
 
     # Train KNN model for text embeddings if not exists
-    assert os.path.exists('models/knn_model_text_embeddings.pkl') is True
+    assert os.path.exists('models/knn_model_text_embeddings.pkl') is False
